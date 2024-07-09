6ed86525c47a2292a1c78beee553f9ef929e3248
atdunwoody <alextd@colostate.edu>
2024-07-09 15:49:58 -0600

    added batch script

---
+++ full_workflow.bat
@@ -0,0 +1,23 @@
+@echo off
+REM Activate the Conda environment
+CALL conda activate geonet
+
+REM Change to the project directory
+CD C:\Users\adunw\OneDrive\Documents\GitHub\GeoNet
+
+REM Execute Python scripts for Nonlinear Filtering, Slope and Curvature
+python pygeonet_nonlinear_filter.py
+python pygeonet_slope_curvature.py
+
+python pygeonet_grass_py3.py
+
+
+REM Execute remaining Python scripts
+python pygeonet_skeleton_definition.py
+python pygeonet_fast_marching.py
+python pygeonet_channel_head_definition.py
+
+REM Deactivate the Conda environment
+CALL conda deactivate
+
+@echo on
